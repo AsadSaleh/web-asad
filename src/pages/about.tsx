0b4d6ffe3e0a97fa4e88ae64a8c7cc56c39bce72
As'ad Saleh Umar <azzzad.s@gmail.com>
2023-04-01 18:40:46 +0700

    feat: add content and some layout structure

---
+++ src/pages/about.tsx
@@ -0,0 +1,36 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Hello I&apos;m porto page</h1>
+      <p>
+        I mainly do front-end and mobile development, using ReactJS,
+        ReactNative, and Flutter; but I am also interested in the backend
+        technology, NodeJS, Golang, Rust, etc. Currently, my go-to stack is
+        NextJS/Remix/CRA, TailwindCSS, React Query, and Prisma. All done in
+        TypeScript. For me, TS is a must. Even my small project uses TS. I take
+        it as an opportunity to learn the type system itself.
+      </p>
+
+      <p>TL;DR</p>
+      <p>
+        Gue lulusan Fisika, pernah ngoding dikit pake bahasa Fortran untuk
+        skripsi. Habis itu gue nganggur setahun lebih setelah gue lulus kuliah.
+        Nah, dari situ gue belajar ngoding. 5 hari seminggu, 8 jam sehari.
+        Selama 6 bulan. Yang gue pelajari mulai dari HTML, CSS, JavaScript, dan
+        terakhir React. Setelah gue bisa React, gue coba bikin 3 website
+        sederhana. 1 buat nampilin informasi cuaca, 1 untuk nampilin
+        infographics, 1 lagi lupa buat apa. Ini kayaknya gue kerjain dalam waktu
+        1 bulan. Ini disebut portofolio ternyata. Dulu gue belum tau. Tapi
+        dengan gue buat ini, temen gue cukup yakin gue bisa mulai kerja. Nah,
+        setelah itu gue mulai internship di kantor temen gue selama 2 bulan.
+        Setelah internship dan cocok, gue mulai kerja sebagai Junior Frontend
+        Developer. Alhamdulillah. Jujur waktu jeda kuliah sampai kerja gue cukup
+        lama. 1 tahun lebih dan belum jelas arah mau ngapain. Itu rasanya bangun
+        tidur males banget gak tau mau ngapain. Tapi pas udah ada kegiatan
+        belajar ngoding rutin tersebut, gue kayak punya semangat baru. Walaupun
+        belum tau bakal kerja sebagai software engineer, tapi dulu seneng aja
+        waktu mempelajari JavaScript, React, cara kerja internet, API, dsb.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/porto.tsx
@@ -0,0 +1,38 @@
+export default function Porto() {
+  return (
+    <div>
+      <h1>Hello I&apos;m porto page</h1>
+
+      <div>
+        <p>https://github.com/AsadSaleh/drop-em</p>
+        <p>An NPM Library for creating Drag & Drop React component</p>
+      </div>
+
+      <div>
+        <p>https://github.com/AsadSaleh/time-ui</p>
+        <p>https://timezone-jakarta-ui.netlify.app/</p>
+        <p>A simple web for managing your preferred global clocks</p>
+      </div>
+
+      <div>
+        <p>https://infradigitalfoundation.netlify.app/</p>
+        <p>A website for Infradigital Foundation company profile</p>
+      </div>
+
+      <div>
+        <p>https://iyada.id/</p>
+        <p>A website for PT Iyada company profile</p>
+      </div>
+
+      <div>
+        <p></p>
+        <p>A sample React Native app which follows Flip&apos;s delightful UI</p>
+      </div>
+
+      <div>
+        <p>https://github.com/AsadSaleh/web-asad</p>
+        <p>This website</p>
+      </div>
+    </div>
+  );
+}
