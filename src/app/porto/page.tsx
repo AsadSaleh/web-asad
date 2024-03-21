999658328baf337a0d7d6a0c55c992d5b5d0fb7a
As'ad Saleh Umar <azzzad.s@gmail.com>
2024-03-21 19:10:03 +0700

    Add tangga_belajar to porto

--- src/app/porto/page.tsx
+++ src/app/porto/page.tsx
@@ -23,83 +23,20 @@ export default function Porto() {
 
       <section className="max-w-3xl mx-auto">
         <h1 className="text-3xl dark:text-white">Portfolio</h1>
-        <h4 className="text-xl text-slate-700 dark:text-slate-500">
+        <h2 className="text-xl text-slate-700 dark:text-slate-500">
           My work & hobby projects are listed here
-        </h4>
+        </h2>
 
-        <h5 className="mt-10 text-xl dark:text-white">
+        <h3 className="mt-10 text-xl dark:text-white">
           Selected Work Projects
-        </h5>
-        <div className="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4">
-          {/* Lumbung Dana */}
-          <PortoCard
-            title="Lumbung Dana web app"
-            desc="Aplikasi web untuk mengelola dana pinjaman peer-to-peer"
-            imageUrl="/images/lumbung_dana.png"
-            links={{
-              app: "https://lumbungdana.co.id/",
-            }}
-            year="2018"
-          />
-
-          {/* Moodah */}
-          {/* <PortoCard
-              title="Moodah mobile app"
-              desc="Aplikasi Android & iOS untuk mengelola ERP perusahaan yang terhubung ke sistem Moodah"
-              links={{}}
-            /> */}
-
-          {/* Transfez */}
-          <PortoCard
-            title="Transfez mobile app"
-            desc="Aplikasi Android & iOS untuk mengirim uang ke luar negeri"
-            links={{
-              app: "https://play.google.com/store/apps/details?id=com.indokoala.transfez&hl=id&gl=US",
-            }}
-            year="2019"
-          />
-
-          {/* IDF */}
-          <PortoCard
-            title="Yayasan IDF site"
-            desc="Website untuk mengenalkan Yayasan IDF kepada masyarakat"
-            imageUrl="/images/idf.png"
-            links={{ app: "https://infradigitalfoundation.netlify.app/" }}
-            year="2020"
-          />
-
-          {/* Iyada */}
-          <PortoCard
-            title="PT Iyada company profile site"
-            desc="Website untuk mengenalkan PT Iyada kepada masyarakat"
-            imageUrl="/images/iyada.png"
-            links={{ app: "https://iyada.id/" }}
-            year="2020"
-          />
-
-          {/* Pelajar */}
-          <PortoCard
-            title="Pelajar mobile app"
-            desc="Aplikasi mobile untuk wali murid mengelola seluruh keperluan digital siswa"
-            imageUrl="/images/pelajar.png"
-            links={{
-              app: "https://play.google.com/store/apps/details?id=io.infradigital.app.pelajar&hl=en_US",
-            }}
-            year="2021"
-          />
-
-          {/* Pordi */}
-          <PortoCard
-            title="Portal Digital web app"
-            desc="Aplikasi web untuk staf sekolah mengelola seluruh keperluan digital sekolahnya"
-            imageUrl="/images/pordi.png"
-            links={{
-              app: "https://pordi.infradigital.io/",
-            }}
-            year="2021"
-          />
-
-          {/* Pager */}
+        </h3>
+        <h4 className="dark:text-slate-400">
+          Ini adalah kerjaan kantor. Biasanya kami buat bersama tim. Tapi ada
+          juga beberapa yang buat sendiri{" "}
+          <span className="italic">from scratch</span>, baru{" "}
+          <span className="italic">later</span> dikerjakan bersama tim.
+        </h4>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
           <PortoCard
             title="Pager web app"
             desc="Aplikasi web SaaS untuk mengelola notifikasi aplikasi web3"
@@ -109,11 +46,87 @@ export default function Porto() {
             }}
             year="2023"
           />
+          <PortoCard
+            title="Pelajar mobile app"
+            desc="Aplikasi mobile untuk wali murid mengelola seluruh keperluan digital siswa"
+            imageUrl="/images/pelajar.png"
+            links={{
+              app: "https://play.google.com/store/apps/details?id=io.infradigital.app.pelajar&hl=en_US",
+            }}
+            year="2021"
+          />
+          <PortoCard
+            title="Portal Digital web app"
+            desc="Aplikasi web untuk staf sekolah mengelola seluruh keperluan digital sekolahnya"
+            imageUrl="/images/pordi.png"
+            links={{
+              app: "https://pordi.infradigital.io/",
+            }}
+            year="2021"
+          />
+          <PortoCard
+            title="Yayasan IDF site"
+            desc="Website untuk mengenalkan Yayasan IDF kepada masyarakat"
+            imageUrl="/images/idf.png"
+            links={{ app: "https://infradigitalfoundation.netlify.app/" }}
+            year="2020"
+          />
+          <PortoCard
+            title="PT Iyada company profile site"
+            desc="Website untuk mengenalkan PT Iyada kepada masyarakat"
+            imageUrl="/images/iyada.png"
+            links={{ app: "https://iyada.id/" }}
+            year="2020"
+          />
+          <PortoCard
+            title="Moodah mobile app"
+            desc="Aplikasi Android & iOS untuk mengelola ERP perusahaan yang terhubung ke sistem Moodah (Service Discontinued)"
+            links={{}}
+            year="2019"
+          />
+          <PortoCard
+            title="Transfez mobile app"
+            desc="Aplikasi Android & iOS untuk mengirim uang ke luar negeri"
+            links={{
+              app: "https://play.google.com/store/apps/details?id=com.indokoala.transfez&hl=id&gl=US",
+            }}
+            year="2019"
+          />
+          <PortoCard
+            title="Lumbung Dana web app"
+            desc="Aplikasi web untuk mengelola dana pinjaman peer-to-peer"
+            imageUrl="/images/lumbung_dana.png"
+            links={{
+              app: "https://lumbungdana.co.id/",
+            }}
+            year="2018"
+          />
         </div>
 
-        <h5 className="mt-10 text-xl dark:text-white">Hobby Projects</h5>
+        <h3 className="mt-10 text-xl dark:text-white">Hobby Projects</h3>
+        <h4 className="dark:text-slate-400">
+          Ini adalah kerjaan yang saya buat sendiri, biasanya saya buat karena
+          keresahan atas suatu masalah. Misal Pajakin: karena ngitung pajak
+          susah. Atau TimeToSync, karena pengen tau cepet beberapa waktu
+          timezone temen ketika kerja remote.
+        </h4>
         <div className="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4 ">
-          {/* GlobalTimeDetector */}
+          <PortoCard
+            title="Tangga Belajar"
+            desc="Next-Gen Indonesia's Coding Learning Platform"
+            imageUrl="/images/tangga_belajar.png"
+            links={{ app: "https://tanggabelajar.id/" }}
+            year="2024"
+          />
+          <PortoCard
+            title="Personal site"
+            desc="My personal website built with Next.js and TailwindCSS"
+            links={{
+              github: "https://github.com/AsadSaleh/web-asad",
+            }}
+            imageUrl="/images/web-asad.png"
+            year="2023"
+          />
           <PortoCard
             title="Global Time Detector"
             desc="A simple app to detect a random time and convert it to your timezone"
@@ -124,8 +137,6 @@ export default function Porto() {
             }}
             year="2023"
           />
-
-          {/* TimeToSync */}
           <PortoCard
             title="TimeToSync"
             desc="A beatiful clock app for team sync across the globe"
@@ -136,8 +147,6 @@ export default function Porto() {
             }}
             year="2022"
           />
-
-          {/* Pajakin */}
           <PortoCard
             title="Pajakin"
             desc="Free tax calculator for Indonesian worker (Pph 21)"
@@ -148,8 +157,6 @@ export default function Porto() {
             }}
             year="2023"
           />
-
-          {/* Is it Retina? */}
           <PortoCard
             title="Is it Retina?"
             desc="Apakah layar kamu 'Retina Display'?"
@@ -160,8 +167,6 @@ export default function Porto() {
             }}
             year="2023"
           />
-
-          {/* React native */}
           <PortoCard
             title="Flip mobile app (ui only)"
             desc="A sample React Native app which follows Flip's delightful UI"
@@ -171,25 +176,12 @@ export default function Porto() {
             }}
             year="2022"
           />
-
-          {/* Drop em */}
           <PortoCard
             title="Drop Em"
             desc="Component example for creating Drag & Drop file in React"
             links={{ github: "https://github.com/AsadSaleh/drop-em" }}
             imageUrl=""
             year="2021"
-          />
-
-          {/* This website */}
-          <PortoCard
-            title="This website"
-            desc="My personal website built with Next.js and TailwindCSS"
-            links={{
-              github: "https://github.com/AsadSaleh/web-asad",
-            }}
-            imageUrl="/images/web-asad.png"
-            year="2023"
           />
         </div>
       </section>
