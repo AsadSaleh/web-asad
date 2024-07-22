c2c8647dd7f724e6915f87d9959811392a6ecd5e
As'ad Saleh Umar <azzzad.s@gmail.com>
2024-07-22 12:04:22 +0700

    refactor: revamp page

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -79,77 +79,107 @@ export default function Home() {
         </div>
       </div>
 
-      <div
-        data-scrambled={scrambled}
-        className="px-2 mt-20 animate-none data-[scrambled=true]:animate-float-2 data-[scrambled=true]:mt-40"
-      >
-        <Link href="/porto">
-          <div
-            data-scrambled={scrambled}
-            className="rotate-0 data-[scrambled=true]:rotate-6 group p-1 rounded-lg bg-gradient-to-r from-violet-700 to-blue-600 active:scale-95 transition-all"
-          >
-            <div className="w-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
-              <h2 className="text-xl md:text-3xl flex items-center gap-2 text-white dark:text-white">
-                <span>Portfolio</span>
-                <ArrowRightIcon className="group-hover:translate-x-1 transition-all dark:text-white" />
-              </h2>
-              <p className="text-sm md:text-lg text-white dark:text-slate-300">
-                Let&apos;s see As&apos;ad&apos;s playing cards
-              </p>
-            </div>
-          </div>
-        </Link>
-      </div>
-
-      <div
-        data-scrambled={scrambled}
-        className="px-2 mt-6 animate-none data-[scrambled=true]:animate-float"
-        style={{ animationDelay: "0.75s" }}
-      >
-        <Link href="/about">
-          <div
-            data-scrambled={scrambled}
-            className="rotate-0 data-[scrambled=true]:-rotate-3 group p-1 rounded-lg bg-gradient-to-r from-red-700 to-orange-600 active:scale-95 transition"
-          >
-            <div className="w-full h-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
-              <h2 className="text-xl md:text-3xl flex items-center gap-2 text-white dark:text-white">
-                <span>About</span>
-                <ArrowRightIcon className="group-hover:translate-x-1 transition-all dark:text-white" />
-              </h2>
-              <p className="text-sm md:text-lg text-white dark:text-slate-300">
-                Read a boring story about my journey
-              </p>
-            </div>
-          </div>
-        </Link>
-      </div>
-
-      <div
-        data-scrambled={scrambled}
-        className="px-2 mt-6 animate-none data-[scrambled=true]:animate-float data-[scrambled=true]:mt-14"
-        style={{ animationDelay: "1.5s" }}
-      >
-        <a
-          href="https://asadghanim.notion.site/be2974dfd64245ae9d0023d947d296ef?v=d9bffec2c5c541218f9df5cba411bae5"
-          target="_blank"
-          rel="noopener noreferrer"
+      <section className="grid grid-cols-2 mt-20 gap-4 px-8 lg:px-0 items-stretch">
+        <div
+          data-scrambled={scrambled}
+          className=" animate-none data-[scrambled=true]:animate-float-2 data-[scrambled=true]:mt-40"
         >
-          <div
-            data-scrambled={scrambled}
-            className="rotate-0 data-[scrambled=true]:rotate-[185deg] group p-1 rounded-lg bg-gradient-to-r from-green-600 to-teal-800 active:scale-95 transition"
-          >
-            <div className="w-full h-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
-              <h2 className="text-xl md:text-3xl flex gap-1 text-white dark:text-white">
-                <span>Blogs</span>
-                <ArrowUpRightIcon className="group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
-              </h2>
-              <p className="text-sm md:text-lg text-white dark:text-slate-300">
-                Discover what&apos;s inside my head
-              </p>
+          <Link href="/porto">
+            <div
+              data-scrambled={scrambled}
+              className="rotate-0 data-[scrambled=true]:rotate-6 group p-1 rounded-lg bg-gradient-to-r from-violet-700 to-blue-600 active:scale-95 transition-all"
+            >
+              <div className="w-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
+                <h2 className="text-xl md:text-3xl flex items-center gap-2 text-white dark:text-white">
+                  <span>Portfolio</span>
+                  <ArrowRightIcon className="group-hover:translate-x-1 transition-all dark:text-white" />
+                </h2>
+                <p className="text-sm md:text-base text-slate-200 dark:text-slate-300">
+                  Let&apos;s see As&apos;ad&apos;s playing cards
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
-      </div>
+          </Link>
+        </div>
+
+        <div
+          data-scrambled={scrambled}
+          className=" animate-none data-[scrambled=true]:animate-float data-[scrambled=true]:mt-10"
+          style={{ animationDelay: "0.75s" }}
+        >
+          <Link href="/about">
+            <div
+              data-scrambled={scrambled}
+              className="rotate-0 data-[scrambled=true]:-rotate-3 group p-1 rounded-lg bg-gradient-to-r from-red-700 to-orange-600 active:scale-95 transition"
+            >
+              <div className="w-full h-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
+                <h2 className="text-xl md:text-3xl flex items-center gap-2 text-white dark:text-white">
+                  <span>About</span>
+                  <ArrowRightIcon className="group-hover:translate-x-1 transition-all dark:text-white" />
+                </h2>
+                <p className="text-sm md:text-base text-slate-200 dark:text-slate-300">
+                  Read story about my journey
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div
+          data-scrambled={scrambled}
+          className=" animate-none data-[scrambled=true]:animate-float data-[scrambled=true]:mt-14"
+          style={{ animationDelay: "1.5s" }}
+        >
+          <a
+            href="https://asadghanim.notion.site/be2974dfd64245ae9d0023d947d296ef?v=d9bffec2c5c541218f9df5cba411bae5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div
+              data-scrambled={scrambled}
+              className="rotate-0 data-[scrambled=true]:rotate-[160deg] group p-1 rounded-lg bg-gradient-to-r from-green-600 to-teal-800 active:scale-95 transition"
+            >
+              <div className="w-full h-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
+                <h2 className="text-xl md:text-3xl flex gap-1 text-white dark:text-white">
+                  <span>Blogs</span>
+                  <ArrowUpRightIcon className="group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
+                </h2>
+                <p className="text-sm md:text-base text-slate-200 dark:text-slate-300">
+                  Discover what&apos;s inside my head
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div
+          data-scrambled={scrambled}
+          className=" animate-none data-[scrambled=true]:animate-float"
+          style={{ animationDelay: "2.2s" }}
+        >
+          <a
+            href="https://www.tanggabelajar.id/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div
+              data-scrambled={scrambled}
+              className="rotate-0 data-[scrambled=true]:rotate-[45deg] group p-1 rounded-lg bg-gradient-to-r from-orange-600 to-yellow-500 active:scale-95 transition"
+            >
+              <div className="w-full h-full p-4 md:px-8 md:py-4 dark:bg-black rounded-lg group-hover:bg-transparent transition-all">
+                <h2 className="text-xl md:text-3xl flex gap-1 text-white dark:text-white">
+                  <span>Learn to Code</span>
+                  <ArrowUpRightIcon className="group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
+                </h2>
+                <p className="text-sm md:text-base text-slate-200 dark:text-slate-300">
+                  Free coding course
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </section>
+
       <footer className="mt-20 px-2">
         <p className="text-xs text-center text-stone-400 dark:text-stone-500">
           © {new Date().getFullYear()} As&apos;ad Ghanim. All rights reserved.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import { Metadata } from "next";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
     "Home - As'ad Personal Site - Software Engineer terbaik sepanjang masa!",
   description: "Welcome to As'ad's personal site. A software engineer.",
+  metadataBase: new URL("https://asadghanim.vercel.app/"),
 };
 
 export default function RootLayout({
@@ -15,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
   );
